9a3223abe9553a64449dc94f78fab77f26e20455
saran-pankaew <pankaewf@gmail.com>
2025-10-09 16:51:09 +0200

    Remove `succession_diagram` from original branch

--- src/astrologics/__init__.py
+++ src/astrologics/__init__.py
@@ -12,8 +12,6 @@ from .model_attractors import attractors
 from .model_simulation import simulation
 from .trajectory_clustering import trajectory
 from .logical_rules_processing import logic
-from .succession_diagram import SuccessionDiagram
-# from .logic_analysis import analysis
 
 class LogicEnsemble:
     def __init__(self, path, project_name):
@@ -41,9 +39,9 @@ class LogicEnsemble:
         self.logic = logic(self.path)
         print('Logic object created')
 
-    def create_succession_diagram(self):
-        self.succession_diagram = SuccessionDiagram(self.path)
-        print('Succession diagram object created')
+    #def create_succession_diagram(self):
+    #    self.succession_diagram = SuccessionDiagram(self.path)
+    #    print('Succession diagram object created')
     
     def __repr__(self):
         print(f'LogicEnsemble object for the project {self.project}')
